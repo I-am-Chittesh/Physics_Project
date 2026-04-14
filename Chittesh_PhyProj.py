c60fbb87c339975933717fd0f2935e3d3634cd89
I-am-Chittesh <iamchitteshd@gmail.com>
2026-04-15 01:09:50 +0530

    final

--- Chittesh_PhyProj.py
+++ Chittesh_PhyProj.py
@@ -6,29 +6,19 @@ N = 10
 wc = 1.0 * 2 * np.pi   
 wa = 1.0 * 2 * np.pi   
 g  = 0.05 * 2 * np.pi  
-
 kappa = 0.05           
 gamma = 0.05           
-
 a  = tensor(destroy(N), qeye(2)) 
 sm = tensor(qeye(N), destroy(2))
-
 H = wc * a.dag() * a + wa * sm.dag() * sm + g * (a.dag() * sm + a * sm.dag())
-
 psi0 = tensor(basis(N, 0), basis(2, 1))
-
 c_ops = []
 c_ops.append(np.sqrt(kappa) * a)  
 c_ops.append(np.sqrt(gamma) * sm) 
-
 tlist = np.linspace(0, 25, 250)
-
 track_op = [sm.dag() * sm]
-
 ideal_result = mesolve(H, psi0, tlist, [], track_op)
-
 noisy_result = mesolve(H, psi0, tlist, c_ops, track_op)
-
 fig, ax = plt.subplots(figsize=(10, 6))
 
 ax.plot(tlist, ideal_result.expect[0], 'b--', linewidth=2, label='Ideal System (Perfect Rabi Oscillations)')
